6969770a23cc2c810ea3ba802e4594c8c983b647
qhotian <qhotian@gmail.com>
2019-09-17 14:13:54 +0800

    fix

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -1,6 +1,8 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 const TcbRouter = require('tcb-router')
+const rp = require('request-promise')
+const BASE_URL = 'http://musicapi.xiecheng.live'
 
 cloud.init()
 const db = cloud.database()
@@ -9,7 +11,7 @@ const playListCollection =  db.collection('playlist')
 // 云函数入口函数
 exports.main = async (event, context) => {
     const app = new TcbRouter({ event })
-    const { page, limit, userInfo } = event
+    const { page, limit, userInfo, playlistId } = event
     app.use(async (ctx, next) => {
         ctx.data = {}
         ctx.data.userInfo = userInfo
@@ -23,6 +25,9 @@ exports.main = async (event, context) => {
         .get()
         result.userInfo = ctx.data.userInfo
         ctx.body = result
+    })
+    app.router('musics', async(ctx) => {
+        ctx.body = await rp(`${BASE_URL}/playlist/detail?id=${parseInt(playlistId)}`).then(res => JSON.parse(res))
     })
     return app.serve()
 }
