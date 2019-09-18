117f9b38b8d06530ac9c47e8cea55a3ea6824f4b
qhotian <qhotian@gmail.com>
2019-09-18 21:21:30 +0800

    发现页

---
+++ miniprogram/components/search/index.js
@@ -0,0 +1,23 @@
+// components/search/index.js
+Component({
+    options: {
+        styleIsolation: 'apply-shared'
+    },
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
