f23d38a5bc2822bd4b63d74edeaa502079d5d31c
qhotian <qhotian@gmail.com>
2019-09-19 14:37:21 +0800

    博客列表

--- cloudfunctions/blog/index.js
+++ cloudfunctions/blog/index.js
@@ -3,14 +3,34 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 
+const TcbRouter = require('tcb-router')
+
+const db = cloud.database()
+
+const blogCollection = db.collection('blog')
+
+const MAX_LIMIT = 100
+
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+    const app = new TcbRouter({ event })
+    const { keyword = '', page = 1, limit = 10 } = event
+    app.router('list', async (ctx, next) => {
+        let w = {}
+        if (keyword.trim()) {
+            w = {
+                content: new db.RegExp({
+                    regexp: keyword,
+                    options: 'i'
+                })
+            }
+        }
+        ctx.body = await blogCollection
+        .where(w)
+        .skip((page - 1) * limit)
+        .limit(limit)
+        .orderBy('createTime', 'desc')
+        .get()
+    })
+    return app.serve()
 }
